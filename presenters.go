5b2dd19634516b966f0de1e78e27f1f97a33e9b6
deltegui <deltegui@hotmail.com>
2020-05-14 15:57:11 +0200

    fixed problems with implementation of HTMLPresenter with new Presenter function

--- presenters.go
+++ presenters.go
@@ -12,8 +12,13 @@ import (
 	"github.com/deltegui/locomotive/vars"
 )
 
+type PresenterMetadata struct {
+	ViewName string
+}
+
 type Presenter interface {
 	Present(data interface{})
+	Present(data interface{}, metadata PresenterMetadata)
 	PresentError(data error)
 }
 
@@ -32,23 +37,32 @@ func getTemplateEngine() *template.Template {
 	return templateEngine
 }
 
-type HTMLMetadata struct {
-	ViewName string
-}
-
 type HTMLPresenter struct {
 	Writer http.ResponseWriter
 }
 
 func (presenter HTMLPresenter) Present(data interface{}) {
-	metadata := data.(HTMLMetadata)
-	if !presenter.render(metadata.ViewName, data) {
+	found := false
+	for skip := 3; skip > 1 && !found; skip-- {
+		view := viewNameFromCallerSkipping(skip)
+		log.Printf("Extracted view name: %s\n", view)
+		if presenter.render(view, data) {
+			found = true
+		}
+	}
+	if !found {
 		log.Fatalln("Cannot find your view")
 	}
 }
 
+func (presenter HTMLPresenter) Present(data interface{}, metadata PresenterMetadata) {
+	if !presenter.render(metadata.ViewName, data) {
+		log.Fatalf("Cannot find view with name: %s\n", metadata.ViewName)
+	}
+}
+
 func (presenter HTMLPresenter) PresentError(caseError error) {
-	presenter.render("error.html", caseError)
+	presenter.Present(caseError, PresenterMetadata{"error.html"})
 }
 
 func (presenter HTMLPresenter) render(view string, data interface{}) bool {
@@ -83,6 +97,10 @@ func (presenter JSONPresenter) Present(data interface{}) {
 	}
 	presenter.Writer.Header().Set("Content-Type", "application/json")
 	presenter.Writer.Write(response)
+}
+
+func (presenter HTMLPresenter) Present(data interface{}, metadata PresenterMetadata) {
+	presenter.Present(data)
 }
 
 func (presenter JSONPresenter) PresentError(caseError error) {
