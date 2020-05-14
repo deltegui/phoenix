a810db3eab01c904549eee279ee1385f5a6cc01c
deltegui <deltegui@hotmail.com>
2020-05-14 23:37:38 +0200

    now presenters are renderers. pure clean architecture implementation

--- renderers.go
+++ renderers.go
@@ -12,14 +12,14 @@ import (
 	"github.com/deltegui/locomotive/vars"
 )
 
-type PresenterMetadata struct {
+type RenderMetadata struct {
 	ViewName string
 }
 
-type Presenter interface {
-	Present(data interface{})
-	PresentWithMeta(data interface{}, metadata PresenterMetadata)
-	PresentError(data error)
+type Renderer interface {
+	Render(data interface{})
+	RenderWithMeta(data interface{}, metadata RenderMetadata)
+	RenderError()
 }
 
 var templateEngine *template.Template = nil
@@ -37,16 +37,16 @@ func getTemplateEngine() *template.Template {
 	return templateEngine
 }
 
-type HTMLPresenter struct {
+type HTMLRenderer struct {
 	Writer http.ResponseWriter
 }
 
-func (presenter HTMLPresenter) Present(data interface{}) {
+func (renderer HTMLRenderer) Render(data interface{}) {
 	found := false
 	for skip := 3; skip > 1 && !found; skip-- {
 		view := viewNameFromCallerSkipping(skip)
 		log.Printf("Extracted view name: %s\n", view)
-		if presenter.render(view, data) {
+		if renderer.renderTemplate(view, data) {
 			found = true
 		}
 	}
@@ -55,18 +55,18 @@ func (presenter HTMLPresenter) Present(data interface{}) {
 	}
 }
 
-func (presenter HTMLPresenter) PresentWithMeta(data interface{}, metadata PresenterMetadata) {
-	if !presenter.render(metadata.ViewName, data) {
+func (renderer HTMLRenderer) RenderWithMeta(data interface{}, metadata RenderMetadata) {
+	if !renderer.renderTemplate(metadata.ViewName, data) {
 		log.Fatalf("Cannot find view with name: %s\n", metadata.ViewName)
 	}
 }
 
-func (presenter HTMLPresenter) PresentError(caseError error) {
-	presenter.PresentWithMeta(caseError, PresenterMetadata{"error.html"})
+func (renderer HTMLRenderer) RenderError(caseError error) {
+	renderer.RenderWithMeta(caseError, RenderMetadata{"error.html"})
 }
 
-func (presenter HTMLPresenter) render(view string, data interface{}) bool {
-	if err := getTemplateEngine().ExecuteTemplate(presenter.Writer, view, data); err != nil {
+func (renderer HTMLRenderer) renderTemplate(view string, data interface{}) bool {
+	if err := getTemplateEngine().ExecuteTemplate(renderer.Writer, view, data); err != nil {
 		log.Print("Error during rendering template: ")
 		log.Println(err)
 		return false
@@ -85,25 +85,25 @@ func viewNameFromCallerSkipping(skip int) string {
 	return fmt.Sprintf("%s.html", strings.ToLower(tokens[len(tokens)-1]))
 }
 
-type JSONPresenter struct {
+type JSONRenderer struct {
 	Writer http.ResponseWriter
 }
 
-func (presenter JSONPresenter) Present(data interface{}) {
+func (renderer JSONRenderer) Present(data interface{}) {
 	response, err := json.Marshal(data)
 	if err != nil {
 		log.Println("Error marshaling data: ", err)
 		return
 	}
-	presenter.Writer.Header().Set("Content-Type", "application/json")
-	presenter.Writer.Write(response)
+	renderer.Writer.Header().Set("Content-Type", "application/json")
+	renderer.Writer.Write(response)
 }
 
-func (presenter JSONPresenter) PresentWithMeta(data interface{}, metadata PresenterMetadata) {
-	presenter.Present(data)
+func (renderer JSONRenderer) PresentWithMeta(data interface{}, metadata RenderMetadata) {
+	renderer.Present(data)
 }
 
-func (presenter JSONPresenter) PresentError(caseError error) {
+func (presenter JSONRenderer) PresentError(caseError error) {
 	presenter.Writer.WriteHeader(http.StatusBadRequest)
 	presenter.Present(caseError)
 }
