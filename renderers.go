917d1005f83c6a128697a7545bd993ce4ec7ceba
deltegui <deltegui@hotmail.com>
2020-05-26 00:34:05 +0200

    refactor renderers

--- renderers.go
+++ renderers.go
@@ -2,25 +2,12 @@ package phoenix
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"runtime"
-	"strings"
 
 	"github.com/deltegui/phoenix/vars"
 )
-
-type RenderMetadata struct {
-	ViewName string
-}
-
-type Renderer interface {
-	Render(data interface{})
-	RenderWithMeta(data interface{}, metadata RenderMetadata)
-	RenderError(data error)
-}
 
 var templateEngine *template.Template = nil
 
@@ -41,28 +28,14 @@ type HTMLRenderer struct {
 	Writer http.ResponseWriter
 }
 
-func (renderer HTMLRenderer) Render(data interface{}) {
-	found := false
-	for skip := 3; skip > 1 && !found; skip-- {
-		view := viewNameFromCallerSkipping(skip)
-		log.Printf("Extracted view name: %s\n", view)
-		if renderer.renderTemplate(view, data) {
-			found = true
-		}
-	}
-	if !found {
-		log.Fatalln("Cannot find your view")
-	}
-}
-
-func (renderer HTMLRenderer) RenderWithMeta(data interface{}, metadata RenderMetadata) {
-	if !renderer.renderTemplate(metadata.ViewName, data) {
-		log.Fatalf("Cannot find view with name: %s\n", metadata.ViewName)
+func (renderer HTMLRenderer) Render(view string, data interface{}) {
+	if !renderer.renderTemplate(view, data) {
+		log.Fatalf("Cannot find view with name: %s\n", view)
 	}
 }
 
 func (renderer HTMLRenderer) RenderError(caseError error) {
-	renderer.RenderWithMeta(caseError, RenderMetadata{"error.html"})
+	renderer.Render("error.html", caseError)
 }
 
 func (renderer HTMLRenderer) renderTemplate(view string, data interface{}) bool {
@@ -72,17 +45,6 @@ func (renderer HTMLRenderer) renderTemplate(view string, data interface{}) bool 
 		return false
 	}
 	return true
-}
-
-func viewNameFromCallerSkipping(skip int) string {
-	pc, _, _, ok := runtime.Caller(skip)
-	if !ok {
-		log.Fatalf("HTML presenter: Cannot obtain runtime caller!")
-	}
-	functionCaller := runtime.FuncForPC(pc)
-	fullName := functionCaller.Name()
-	tokens := strings.Split(fullName, ".")
-	return fmt.Sprintf("%s.html", strings.ToLower(tokens[len(tokens)-1]))
 }
 
 type JSONRenderer struct {
@@ -97,10 +59,6 @@ func (renderer JSONRenderer) Render(data interface{}) {
 	}
 	renderer.Writer.Header().Set("Content-Type", "application/json")
 	renderer.Writer.Write(response)
-}
-
-func (renderer JSONRenderer) RenderWithMeta(data interface{}, metadata RenderMetadata) {
-	renderer.Render(data)
 }
 
 func (renderer JSONRenderer) RenderError(caseError error) {
