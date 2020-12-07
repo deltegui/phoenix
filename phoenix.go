291fcad4481e6ad88029941b6d42d9041251e1aa
deltegui <deltegui@hotmail.com>
2020-12-07 17:04:13 +0100

    chai does everything phoenix does, now is a small library

--- phoenix.go
+++ phoenix.go
@@ -8,87 +8,23 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
-type App struct {
-	Mapper
-	config   *PhoenixConfig
-	Injector *Injector
+func CreateStaticServer() {
+	return http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
 }
 
-func NewApp() App {
-	injector := NewInjector()
-	return App{
-		Mapper: Mapper{
-			router:   mux.NewRouter(),
-			injector: injector,
-		},
-		config: &PhoenixConfig{
-			projectName:        "phoenix",
-			projectVersion:     "0.1.0",
-			enableStaticServer: false,
-			logoFile:           "",
-			onStop:             func() {},
-			onStart:            func(server *http.Server) error { return server.ListenAndServe() },
-		},
-		Injector: injector,
-	}
-}
-
-func (app App) Configure() *PhoenixConfig {
-	return app.config
-}
-
-func (app App) Run(listenURL string) {
-	app.printLogo()
-	app.createStaticServer()
-	app.showEndpoints()
-	app.startListening(listenURL)
-}
-
-func (app App) createStaticServer() {
-	if app.config.isStaticServerEnabled() {
-		s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-		app.Mapper.router.PathPrefix("/static").Handler(s)
-		log.Println("Created static server!")
-	}
-}
-
-func (app App) printLogo() {
-	if app.config.isLogoFileEnabled() {
-		logo, err := ioutil.ReadFile(app.config.logoFile)
-		if err != nil {
-			log.Fatalf("Cannot read logo file: %s\n", err)
-		}
-		fmt.Println(string(logo))
-	}
-	fmt.Print(app.config.formatProjectInfo())
-}
-
-func (app App) startListening(address string) {
-	server := &http.Server{
-		Addr:    address,
-		Handler: app.Mapper.router,
-	}
-	go app.startServer(server)
-	app.waitAndStopServer(server)
-}
-
-func (app App) startServer(server *http.Server) {
-	log.Println("Listening on address: ", server.Addr)
-	log.Println("You are ready to GO!")
-	err := app.config.onStart(server)
+func PrintLogo(logoFile) {
+	logo, err := ioutil.ReadFile(logoFile)
 	if err != nil {
-		log.Fatalln("Error while listening: ", err)
+		log.Fatalf("Cannot read logo file: %s\n", err)
 	}
+	fmt.Println(string(logo))
 }
 
-func (app App) waitAndStopServer(server *http.Server) {
+func WaitAndStopServer(server *http.Server) {
 	done := make(chan os.Signal)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	<-done
@@ -106,27 +42,4 @@ func (app App) waitAndStopServer(server *http.Server) {
 	}
 
 	log.Print("Phoenix exited properly")
-}
-
-func (app App) showEndpoints() {
-	stringEndsWith := func(target, end string) bool {
-		return strings.LastIndex(target, end) == len(target)-1
-	}
-
-	app.Mapper.router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		path, err := route.GetPathTemplate()
-		if err != nil {
-			log.Println("Error while fetching route: ", err)
-			return nil
-		}
-		if stringEndsWith(path, "/") && len(path) != 1 {
-			return nil
-		}
-		methods, err := route.GetMethods()
-		if err != nil {
-			methods = []string{"ROOT ENDPOINT"}
-		}
-		log.Printf("%s Registered endpoint in %s", methods, path)
-		return nil
-	})
 }
