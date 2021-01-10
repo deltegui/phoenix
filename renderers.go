8a2d1d0b9f457bcbecc5f5c35361beefec8b6f9b
deltegui <deltegui@hotmail.com>
2021-01-10 17:16:31 +0100

    added htmlTemplate error view

--- renderers.go
+++ renderers.go
@@ -14,30 +14,46 @@ func getTemplateEngine() *template.Template {
 		return templateEngine
 	}
 	templateEngine = template.Must(template.New("html").ParseGlob("./templates/**/*.html"))
-	log.Printf("Template engine %s\n", templateEngine.DefinedTemplates())
+	log.Printf("Template engine%s\n", templateEngine.DefinedTemplates())
 	return templateEngine
 }
 
 type HTMLPresenter struct {
-	Writer http.ResponseWriter
-	View   string
+	Writer    http.ResponseWriter
+	View      string
+	ErrorView string
 }
 
 func NewHTMLPresenter(writer http.ResponseWriter, view string) HTMLPresenter {
 	return HTMLPresenter{
-		Writer: writer,
-		View:   view,
+		Writer:    writer,
+		View:      view,
+		ErrorView: view,
+	}
+}
+
+func NewHTMLPresenterWithErrView(writer http.ResponseWriter, view string, errView string) HTMLPresenter {
+	return HTMLPresenter{
+		Writer:    writer,
+		View:      view,
+		ErrorView: errView,
 	}
 }
 
 func (renderer HTMLPresenter) Present(data interface{}) {
-	if !renderer.renderTemplate(renderer.View, data) {
+	if !renderer.renderTemplate(renderer.View, map[string]interface{}{
+		"haveErrors": false,
+		"data":       data,
+	}) {
 		log.Fatalf("Cannot find view with name: %s\n", renderer.View)
 	}
 }
 
 func (renderer HTMLPresenter) PresentError(caseError error) {
-	renderer.renderTemplate("error.html", caseError)
+	renderer.renderTemplate(renderer.ErrorView, map[string]interface{}{
+		"haveErrors": true,
+		"err":        caseError,
+	})
 }
 
 func (renderer HTMLPresenter) renderTemplate(view string, data interface{}) bool {
